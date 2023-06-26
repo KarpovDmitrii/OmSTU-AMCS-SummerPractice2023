1ad9221dc27f9ef9d9dd7eb66767e5e37fc081b5
Dmitriy <n_eco@mail.ru>
2023-06-26 14:10:27 +0000

    fsdfsd

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,10 +1,10 @@
 ﻿namespace SquareEquationLib;
-
 public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
         double[] answer;
+        double x1, x2;
         double eps = 1e-9;
         if ((-eps < a && a < eps)||
             double.IsInfinity(a) || double.IsInfinity(b) ||
@@ -18,21 +18,19 @@ public class SquareEquation
         double discriminant = Math.Pow(b, 2) - 4 * c;
         if (discriminant <= -eps)
         {
-            answer =new double[0];
+            answer = new double [0];
             return answer;
         }
-        else if (-eps < discriminant && discriminant < eps)
+        else if (Math.Abs(discriminant)<eps)
         {
-            answer = new double[1];
-            answer[0] = -(b) / 2;
+            answer = new double [1] { -b / 2 };
             return answer;
         }
         else
-        
         {
-            answer = new double[2];
-            answer[0] = -(b + Math.Sign(b) * Math.Sqrt(discriminant)) / 2;
-            answer[1] = c / answer[0];
+            x1 = -(b + Math.Sign(b) * Math.Sqrt(discriminant)) / 2;
+            x2 = c / x1;
+            answer = new double [2] { x1, x2 };
             return answer;
         }
     }
